ab39827cb01c0d066663956bbe289ea8060bf8da
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-13 13:50:34 -0800

    added a single photo component with current comments displaying; next to update comment state

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,9 +4,18 @@
 // 2. Copy of current state
 
 function posts(state = [], action){
-    console.log("The post will change");
-    console.log(state, action);
-    return state;
+    switch(action.type){
+        case 'INCREMENT_LIKES' :
+            const i = action.index;
+            console.log(action);
+            return [
+                ...state.slice(0,i), // before the post we're updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1) // after the post we're updating
+            ];
+        default:
+            return state;
+    }
 }
 
 export default posts;
